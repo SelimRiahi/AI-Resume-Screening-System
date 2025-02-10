8e7cacc8a765bcb46e8dae6471bc6a92e096d245
SelimRiahi <riahiselim13@gmail.com>
2025-02-10 13:01:12 +0100

    Initial React project setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AppRoutes from "./routes";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
